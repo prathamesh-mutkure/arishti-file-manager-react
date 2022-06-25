7ccf2294ed2fd3b05684e2abe90195fe293a18db
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-06-25 20:32:37 +0530

    Added Store

--- src/containers/app.tsx
+++ src/containers/app.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Root from "./routes";
+import { Provider } from "react-redux";
+import store from "../store/index.store";
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   return (
-    <div>
-      <Root />
-    </div>
+    <React.Fragment>
+      <Provider store={store}>
+        <Root />
+      </Provider>
+    </React.Fragment>
   );
 };
 
