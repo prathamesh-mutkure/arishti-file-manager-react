0141e5a0f2f0607f84c672dcc698a8c9d0072a43
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-06-26 18:45:14 +0530

    Removed Proxy

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,9 +1,11 @@
-export const Endpoints = {
-  backendUrl: "http://localhost:8001",
+const BACKEND_URL = process.env.REACT_APP_BACKEND ?? "http://localhost:8001";
 
-  postFile: "/api/file",
-  getFile: "/api/file",
-  getFiles: "/api/files",
-  patchFile: "/api/file",
-  deleteFile: "/api/file",
+export const Endpoints = {
+  backendUrl: BACKEND_URL,
+
+  postFile: `${BACKEND_URL}/api/file`,
+  getFile: `${BACKEND_URL}/api/file`,
+  getFiles: `${BACKEND_URL}/api/files`,
+  patchFile: `${BACKEND_URL}/api/file`,
+  deleteFile: `${BACKEND_URL}/api/file`,
 };
